2e53df5873052589b6d86a748ad23b3da3fca3f2
Huy Nguyễn Vũ <durian@mark2>
2023-06-03 16:00:22 +0700

    Init Demo2

---
+++ app/main.py
@@ -0,0 +1,50 @@
+from typing import Union
+from fastapi import FastAPI
+
+from .db import engine, session_local
+from .problem.api_models.factory import ModelCreator
+from .problem.api_models import Base
+from .problem.schema import RequestSchema
+
+Base.metadata.create_all(
+    bind=engine  # create tables for each model if not existing
+)
+app = FastAPI()
+model_creator = ModelCreator()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.post("/phase-{phase_id}/prob-{prob_id}/predict")
+async def recieve(phase_id: int, prob_id: int,
+                  request_data: RequestSchema
+                  ):
+    session = session_local()
+    api_model = model_creator.create_model(phase_id, prob_id)
+
+    for info in request_data['columns']:
+        # add each record to the db
+        record = {
+            col_name : info[idx]
+            for idx, col_name in enumerate(info)
+        }
+        session.add(
+            api_model(
+                **record,
+                request_id = request_data['id']
+                )
+            )
+        session.commit()
+
+    return {
+        "phase_id" : phase_id,
+        "prob_id" : prob_id,
+        # response with the number of records added to the db
+        'message': f'success: {len(request_data["rows"])} records added to db'
+    }
